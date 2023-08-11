896c409336e8511bc7cafc6e428cc577c220989e
Tahinaniaina <141611800+Tahinaniaina@users.noreply.github.com>
2023-08-11 16:04:30 +0300

    main.c

--- main.c
+++ main.c
@@ -1,20 +1,6 @@
 #include "Struct.h"
 
 int main(){
-    
-    Etudiant *tabEt;
-    int TailleDuTabEt;
-    PC *tabPC;
-    int TailleDuTabPC;
-    FILE *file1,*file2;
-    
-    tabEt = RemplirTabEt(file1,&TailleDuTabEt);
-    tabPC = RemplirTabPC(file1,&TailleDuTabPC);
-    
-    //AjoutInfo(file1,file2);
-    
-    TrierTabEt(tabEt,TailleDuTabEt);
-    EcritureDuFichier(file1,tabEt,tabPC,TailleDuTabEt);
-    
+    AjoutInfo();
     return 0;
 }
